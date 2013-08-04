f84daa219246501c1b7bdfc62c935e215e0b568f
hanrylhf <hanrylhf@gmail.com>
2013-08-04 22:22:30 +0800

    实现延迟回调部分

--- src/SubscibeCenter.cpp
+++ src/SubscibeCenter.cpp
@@ -1,6 +1,7 @@
 #include "SubscibeCenter.h"
 
 #include <Windows.h>
+#include <list>
 
 namespace wmp
 {
@@ -13,7 +14,26 @@ void SubscibeCenter::Release()
 
 void SubscibeCenter::OnTimer( wmp::base::ui32 timer_id, wmp::base::ttime now )
 {
-
+	wmp::base::ui64 tick_count = ::GetTickCount();
+	
+	std::list<wmp::base::ui64> lstremove;
+	for(tmap_tickcount2event::iterator it = m_delay_event.begin(); it != m_delay_event.end(); ++it)
+	{
+		if ( it->first < tick_count )
+		{
+			break;
+		}
+		tvecevent& rvecenent = it->second;
+		for(tvecevent::iterator itevt = rvecenent.begin(); itevt != rvecenent.end(); ++itevt)
+		{
+			DoPostEvent((*itevt).event_id, (*itevt).pData, (*itevt).len);
+		}
+		lstremove.push_back(it->first);
+	}
+	for(std::list<wmp::base::ui64>::iterator it = lstremove.begin(); it != lstremove.end(); ++it)
+	{
+		m_delay_event.erase(*it);
+	}
 }
 
 bool SubscibeCenter::Subscibe( IObserver& observer, wmp::base::ui16 event_id, wmp::base::ui8 index, const char* desc )
@@ -93,16 +113,29 @@ bool SubscibeCenter::PostVote( wmp::base::ui16 event_id, char* pData, wmp::base:
 		tmap_voter2desc& voter2desc = *it;
 		for(tmap_voter2desc::iterator it_voter = voter2desc.begin(); it_voter != voter2desc.end(); ++it_voter)
 		{
-			(it_voter->first)->OnVoter(event_id, pData, len);
+			if (!(it_voter->first)->OnVoter(event_id, pData, len))
+			{
+				return false;
+			}
 		}
 	}
 	m_loop_voter = false;
-	return false;
+	return true;
 }
 
 void SubscibeCenter::DoPostEvent( wmp::base::ui16 event_id, char* pData, wmp::base::ui32 len )
 {
-
+	tvecobserver& rvecobserver = m_mapobserver[event_id];
+	m_loop_observer = true;
+	for (tvecobserver::iterator it = rvecobserver.begin(); it != rvecobserver.end(); ++it)
+	{
+		tmap_observer2desc& observer2desc = *it;
+		for(tmap_observer2desc::iterator it_observer = observer2desc.begin(); it_observer!= observer2desc.end(); ++it_observer)
+		{
+			(it_observer->first)->OnEvent(event_id, pData, len);
+		}
+	}
+	m_loop_observer = false;
 }
 
 }
